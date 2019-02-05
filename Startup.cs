914192d03f956d73019306f87758c7c14daf36e7
amit <amit@sonorasoftware.com>
2019-02-05 16:30:08 +0530

    Added support for CustomerName

--- Startup.cs
+++ Startup.cs
@@ -79,7 +79,7 @@ namespace DotNetCoreSqlDb
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=About}/{id?}");
             });
         }
     }
